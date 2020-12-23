412f76f61787b4dab1af54450bbe5f5ac72643f7
mdaw <mdaw@protonmail.com>
2020-12-23 23:20:18 +0100

    simplify template

--- implementations/template.cpp
+++ implementations/template.cpp
@@ -7,21 +7,10 @@ typedef vector<ll> vl;
 typedef vector<vl> vvl;
 typedef pair<int, int> pi;
 typedef vector<pi> vpi;
-double EPS = 1e-9;
-int INF = 1000000005;
-long long INFL = 1000000000000000005LL;
-double PI = acos(-1);
-int dirx[8] = {-1, 0, 0, 1, -1, -1, 1, 1};
-int diry[8] = {0, 1, -1, 0, -1, 1, -1, 1};
-#define FOR(a, b, c) for (int a = b; a < c; ++a)
-#define SORT(v) sort(v.begin(), v.end())
-#define REVERSE(v) reverse(v.begin(), v.end())
-#define VALUE(x) cerr << #x << " = " << x << endl
-#define DEBUG(x) cerr << x << endl
-#define DEBUG_V(a) for_each(ALL(a), [&](auto v) { VALUE(v); })
-#define ALL(v) v.begin(), v.end()
+
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
+
 }
