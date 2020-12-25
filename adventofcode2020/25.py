388cc23b80c5323fec263d45a951777491ed3b41
mdaw <mdaw@protonmail.com>
2020-12-25 11:23:21 +0100

    simplify

--- adventofcode2020/25.py
+++ adventofcode2020/25.py
@@ -1,9 +1,4 @@
-
 mod = 20201227
-
-# card = 5764801
-# door = 17807724
-
 card = 1327981
 door = 2822615
 
@@ -20,15 +15,8 @@ def find_loop(value):
 card_loop = find_loop(card)
 door_loop = find_loop(door)
 
-c = 1
-for i in range(door_loop):
-    c = (c * card) % mod
-
-
-d = 1
-for i in range(card_loop):
-    d = (d * door) % mod
-
-
+c = pow(card, door_loop, mod)
+d = pow(door, card_loop, mod)
 assert c == d
+
 print(c)
